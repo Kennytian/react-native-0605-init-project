426b9175a80e3293f867c394be15b7406c921b91
Kennytian <kt.tian@gmail.com>
2019-08-14 10:53:08 +0800

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 160,
+  tabWidth: 2,
+  jsxBracketSameLine: true
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+# React Native 0.60.5 initialize project
+
+已设置好如下配置：
+
+- Android Hermes（爱马仕引擎）
+- Android O 网络请求
+- TypeScript 语法
+- prettier 美化参数
